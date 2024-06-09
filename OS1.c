5d6f7ce6a991de033c91503e6bf851c7b5f0de8a
Lyprandos Liatsos <102907631+Liprandipus@users.noreply.github.com>
2024-06-09 23:20:07 +0300

    Update OS1.c

--- OS1.c
+++ OS1.c
@@ -1,95 +1,93 @@
-
-
 /*
-Γράψτε ένα πρόγραμμα C με τις κατάλληλες εντολές fork() ώστε να δημιουργηθούν συνολικά (μαζί με το κυρίως
-πρόγραμμα) επτά διεργασίες, με την ακόλουθη δομή (δέντρο) συγγένειας:
+Write a C program with the appropriate fork() commands to create a total (including the main
+program) seven processes, with the following kinship structure (tree):
 P0
  / \
  P1 P2
  / \ / \
 P3 P4 P5 P6
-Στη συνέχεια (αφού δημιουργηθούν δηλαδή όλες οι διεργασίες που υπαγορεύονται από το παραπάνω δέντρο), ως
-κύριο μέρος της εργασίας της κάθε διεργασία Pi θα πρέπει να τυπώνει ένα μήνυμα στην οθόνη που να δηλώνει το
-όνομά της (Pi), το PID της και το PPID της. Επιπλέον, το πρόγραμμά σας θα πρέπει να συμπεριλαμβάνει τις
-κατάλληλες εντολές αναμονής-επικοινωνίας ώστε να ικανοποιούνται οι ακόλουθοι περιορισμοί-απαιτήσεις:
-(α) Η διεργασία P0 πριν ολοκληρώσει την εκτέλεσή της θα πρέπει να περιμένει την ολοκλήρωση της διεργασίας P2.
-Με το πέρας της εκτέλεσής της η P0 θα πρέπει επίσης να αντικαθίσταται από την εντολή ps.
-(β) Η διεργασία P1 πριν ολοκληρώσει την εκτέλεσή της θα πρέπει αρχικά να περιμένει να λάβει ένα μήνυμα 'hello
-from your child' από τη διεργασία P3 (και να το τυπώνει στην οθόνη). Στη συνέχεια θα πρέπει να περιμένει επίσης
-την ολοκλήρωση της διεργασίας P4 και να τυπώνει το exit value αυτής.
-(γ) Η διεργασία P2 πριν ολοκληρώσει την εκτέλεσή της θα πρέπει να περιμένει την ολοκλήρωση ενός εκ των δύο
-παιδιών της και να τυπώνει ποιο είναι αυτό (το PID δηλαδή της διεργασίας-παιδιού που ολοκληρώθηκε).
-Επεκτείνατε επίσης το πρόγραμμά σας έτσι ώστε η διεργασία P2 να δημιουργεί Ν διεργασίες/άμεσα παιδιά (αντί για
-τις δύο μόνο διεργασίες/άμεσα παιδιά P5,P6 που φαίνεται να δημιουργεί στο παραπάνω σχήμα), όπου το N θα
-δίνεται από το χρήστη. Κάθε μία από τις Ν διεργασίες που θα δημιουργηθούν θα πρέπει να τυπώνει απλά ένα
-διαγνωστικό μήνυμα με το PID της και το PPID της και να τερματίζει.
-Είναι πιθανό κατά την εκτέλεση του προγράμματός σας να δημιουργηθούν orphan ή zombie διεργασίες; Και ποιες
-μπορεί να είναι αυτές; Σχολιάστε / τεκμηριώστε επαρκώς την απάντησή σας.
+Then (after all the processes dictated by the above tree have been created), as
+main part of its work each Pi process should print a message on the screen indicating the
+its name (Pi), its PID and its PPID. In addition, your program should include the
+appropriate wait-command commands to satisfy the following constraints-requirements:
+(a) Process P0 must wait for process P2 to complete its execution before completing its execution.
+Upon completion of its execution, P0 should also be replaced by the ps instruction.
+(b) Process P1 before completing its execution should first wait to receive a message 'hello
+from your child' from process P3 (and print it to the screen). Then it should also wait for
+the completion of process P4 and print its exit value.
+(c) Process P2 should wait for the completion of one of the two
+of one of its children and print which one (i.e. the PID of the completed child process).
+You also extend your program so that process P2 creates N processes/immediate children (instead of
+the only two processes/immediate children P5,P6 that it appears to create in the above figure), where N would
+is given by the user. Each of the N processes created should simply print a
+diagnostic message with its PID and PPID and terminate.
+Is it possible during the execution of your program that orphan or zombie processes will be created? And which ones
+might they be? Comment / adequately document your answer.
 */
 
-//Απάντηση :
+//Answer :
 
 #include <stdio.h>
 #include <stdlib.h> 
 #include <unistd.h>
 #include <sys/wait.h> 
- // Τρέχει ήδη με την εκτέλεση του προγραμμάτος η γονική διεργασία 0
- // Πριν περάσουμε στο κύριο πρόγραμμα, θα ήθελα να κάνω κάποια σχόλια μιας και δε θέλω να επαναλαμβάνω τα σχόλια στον κώδικα προκειμένου να μην είναι κουραστικό
- // Η fork() αν επιστρέψει 0, τότε σημαίνει πως δημιουργείται παιδί. Αν δώσει >0 τότε σημαίνει πως είμαστε στην γονική διεργασία. Αν δώσει -1, τότε ειναι error
- // Η δήλωση pid_t είναι ακεραίος ο οποίος αρμόζει καλύτερα στη δήλωση και την δημιουργία των fork()
- // Η waitpid() δέχεται ως πρώτο όρισμα την διεργασία, το status της και το option. 
+ // Already running with the program execution the parent process 0
+ // Before we get to the main program, I would like to make some comments since I don't want to repeat the comments in the code in order to make it not tedious
+ // If fork() returns 0, then it means that a child is created. If it returns >0 then it means we are in the parent process. If it returns -1, then it's an error
+ // The pid_t declaration is an integer which is better suited to the declaration and creation of fork()
+ // waitpid() takes as first argument the process, its status and option. 
 int main() {
-    pid_t pid1, pid2, pid3, pid4, pid5, pid6; // δήλωση των 6 διεργασιών που ζητάει η εκφώνηση
-    int status; // δήλωση ένος status για την αναμόνη της διεργασίας 4 
+    pid_t pid1, pid2, pid3, pid4, pid5, pid6; // declaration of the 6 processes requested by the call
+    int status; // declaration of a status for the anonymous process 4 
 
-    pid1 = fork(); //δημιουργία της πρώτης διεργασίας
-    if (pid1 == 0) { // έλεγχος αν το fork() γυρνάει παιδί
-        pid3 = fork(); //αν γυρνάει παιδί, τότε κάνει fork() για τα παιδιά της 1
-        if (pid3 == 0) { // αν γυρνάει παιδί, τότε
-            printf("P3 with pid %d and ppid %d\n", getpid(), getppid()); // εμφάνισε pid και ppid της 3
+    pid1 = fork(); //create the first process
+    if (pid1 == 0) { // check if fork() returns child
+        pid3 = fork(); //if it returns a child, then fork() for the children of 1
+        if (pid3 == 0) { // if it returns a child, then
+            printf("P3 with pid %d and ppid %d\n", getpid(), getppid()); // display pid and ppid of 3
         }
-        else { // αλλιώς 
-            waitpid(pid3, NULL, 0);  // περιμένει να ολοκληρωθεί η 3 με την εντολή waitpid 
-            pid4 = fork(); // δημιουργία της διεργασία 4
-            if (pid4 == 0) { // αν δώσει παιδί η fork()
-                printf("P4 with pid %d and ppid %d\n", getpid(), getppid()); // εμφάνισε pid και ppid της p4
-                exit(123);  // και κάνει exit απο την 4 με ένα συγκεκριμένο value προκειμένου να περάσει το value της στην waitpid()
+        else { // else 
+            waitpid(pid3, NULL, 0); // wait for 3 to complete with waitpid 
+            pid4 = fork(); // create process 4
+            if (pid4 == 0) { // if fork() gives a child
+                printf("P4 with pid %d and ppid %d\n", getpid(), getppid()); // display pid and ppid of p4
+                exit(123); // and exit from 4 with a specific value in order to pass its value to waitpid()
             }
             else {
-                waitpid(pid4, &status, 0);  // περιμένει την 4 να ολοκληρωθεί και στο status θα περαστεί η τιμή exit value της 4. (status = 123)
-                printf("P1 with pid %d and ppid %d received message 'hello from your child '\n", getpid(), getppid());  // εμφανίζει το μήνυμα που έλαβε μαζί
-                // με το pid και το ppid της 1
-                printf("P1 with pid %d and ppid %d received exit status of P4: %d\n", getpid(), getppid(), WEXITSTATUS(status)); // εμφανίζει ξανά 
-                // το pid και το ppid της 1 και το exit status της 4 ( απο την exit(123))
+                waitpid(pid4, &status, 0); // wait for 4 to complete and the status will be passed the exit value of 4. (status = 123)
+                printf("P1 with pid %d and ppid %d received message 'hello from your child '\n", getpid(), getppid()); // display the message received together
+                // with the pid and ppid of 1
+                printf("P1 with pid %d and ppid %d received exit status of P4: %d\n", getpid(), getppid(), WEXITSTATUS(status)); // display again 
+                // the pid and ppid of 1 and the exit status of 4 ( from exit(123))
             }
         }
     }
-    else if (pid1 > 0) { // αλλιώς αν βρισκόμαστε ακόμη στη γονική εφόσον η 1 φέρει τιμή >1
-        pid2 = fork(); // κανε fork() και δημιούργησε τη 2
-        if (pid2 == 0) { // αν δώσει παιδί το fork()
-            int n; // δήλωσε έναν ακέραιο 
-            printf("Enter the number of processes to create for P2: "); // εκτύπωσε 
-            scanf("%d", &n); // και διάβασε απο το standard input τον αριθμό των διεργασιών που θέλει ο χρήστης να φτιαχτούν
-            for (int i = 0; i < n; i++) { // για κάθε μια απο τις διεργασίες
-                pid_t child_pid = fork(); // δήλωσε μια μεταβλήτη που δημιουργεί ένα παιδί n φορές
-                if (child_pid == 0) { // αν το fork() δώσει παιδί τότε
-                    printf("Child %d of P2 with pid %d and ppid %d\n", i+1, getpid(), getppid()); // εμφάνισε τα pid και ppid του κάθε παιδιού
-                    exit(9);  
+    else if (pid1 > 0) { // else if we are still in the parent if 1 carries a value >1
+        pid2 = fork(); // do fork() and create 2
+        if (pid2 == 0) { // if fork() gives a child
+            int n; // declared an integer 
+            printf("Enter the number of processes to create for P2: "); // print 
+            scanf("%d", &n); // and read from the standard input the number of processes the user wants to create
+            for (int i = 0; i < n; i++) { // for each of the processes
+                pid_t child_pid = fork(); // declare a variable that creates a child n times
+                if (child_pid == 0) { // if fork() returns a child then
+                    printf("Child %d of P2 with pid %d and ppid %d\n", i+1, getpid(), getppid()); // display the pid and ppid of each child
+                    exit(9);;  
                 }
             }
            
         }
-        else if (pid2 > 0) {  // αλλιώς
-            waitpid(pid2, NULL, 0);  // περιμένει να ολοκληρωθεί η 2 προκειμένου να τρέξει η 0
-            printf("P0 with pid %d and ppid %d\n", getpid(), getppid()); // εμφνιζεί pid και ppid της p0
-            system("ps"); // με την εντολή system("ps") εμφανίζουμε την εντολή του terminal ps μέσα στη C. Στην ουσία, μας δίνει την δυνατότητα 
-            // να βάζουμε εντολές του λειτουργικού linux ως εντολές στην C.
+        else if (pid2 > 0) { // else
+            waitpid(pid2, NULL, 0); // wait for 2 to complete in order to run 0
+            printf("P0 with pid %d and ppid %d\n", getpid(), getppid()); // printf("P0 with pid %d and ppid %d\n", getpid(), getppid()); // display pid and ppid of p0
+            system("ps"); // with system("ps") we display the ps terminal command inside C. In essence, it allows us to 
+            // to put linux commands as commands in C.
         }
     }
 
     return 0;
 }
 
-//Κατά το τρέξιμο του προγραμμάτος, διαπυστώνουμε πως στη δημιουργία των N processes, δημιουργούνται orphan processes. Αυτό συμβαίνει καθώς κατά τη δημιουργία
-//άλλου process, ενδεχομένως να μην καθαρίζονται οι buffers . Με άλλα λόγια, το "σκότωμα" μιας διεργασίας να μην είναι ομαλό και έτσι να μην καθαρίζονται 
-// τα απαραίτητα resources με αποτέλεσμα την δημιουργία των orphans.
+//When the program is run, we show that in the creation of N processes, orphan processes are created. This is because when creating
+//all processes, the buffers may not be cleared. In other words, the "kill" of a process may not be smooth and thus the buffers may not be cleared. 
+// the necessary resources resulting in the creation of orphans.
